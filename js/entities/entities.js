c0e983e43ac483c94e9ca8234b3337018935c48d
Joseph E. Roldan <omega6441@gmail.com>
2015-03-09 10:45:59 -0700

    on milestone 3

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -1,1 +1,43 @@
 // TODO
+game.PlayerEntity = me.Entity.extend({
+    init: function(x, y, settings) {
+        this._super(me.Entity, 'init', [x, y, {
+                image: "player",
+                width: 64,
+                height: 64,
+                spritewidth: "64",
+                spriteheight: "64",
+                getShape: function() {
+                    return(new me.Rect(0, 0, 64, 64)).toPolygon();
+                    //sets player size and shape
+                }
+            }]);
+        this.body.setVelocity(5, 20);
+        this.renderable.addAnimation("idle", [78]);
+        this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
+        
+        
+        this.renderable.setCurrentAnimation("idle");
+
+    },
+    update: function(delta) {
+        if (me.input.isKeyPressed("right")) {
+            //sets x position by adding value set in set velocity
+            //multiplying by me.timer.tick 
+            //me.timer.tick smooths movement
+            this.body.vel.x += this.body.accel.x * me.timer.tick;
+        } else {
+            this.body.vel.x = 0;
+        }
+        
+        if(!this.renderable.isCurrentAnimation("walk")){
+            this.renderable.setCurrentAnimation("walk");
+        }
+        
+        this.body.update(delta);
+        
+        this._super(me.Entity, "update", [delta]); 
+        return true;
+    }
+
+});
--- js/resources.js
+++ js/resources.js
@@ -4,9 +4,9 @@ game.resources = [
 	 * @example
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
-        {name:"level01", type:"tmx", src:"data/map/test.tmx"},
-
-
+        {name:"background-tiles", type:"image", src:"data/img/background-tiles.png"},
+     {name:"meta-tiles", type:"image", src:"data/img/meta-tiles.png"},
+{name:"player", type:"image", src:"data/img/orcSpear.png"},
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
@@ -17,6 +17,10 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
+
+     {name:"level01", type:"tmx", src:"data/map/test.tmx"},
+
+
 
 	/* Background music. 
 	 * @example

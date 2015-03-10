9fc8341f9e5a62d8b71367cfa57aaada9465df26
Joseph E. Roldan <omega6441@gmail.com>
2015-03-10 09:18:29 -0700

    made little progress on work cruddy internet

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -15,8 +15,8 @@ game.PlayerEntity = me.Entity.extend({
         this.body.setVelocity(5, 20);
         this.renderable.addAnimation("idle", [78]);
         this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
-        
-        
+
+
         this.renderable.setCurrentAnimation("idle");
 
     },
@@ -26,18 +26,43 @@ game.PlayerEntity = me.Entity.extend({
             //multiplying by me.timer.tick 
             //me.timer.tick smooths movement
             this.body.vel.x += this.body.accel.x * me.timer.tick;
+            this.flipX(true);
         } else {
             this.body.vel.x = 0;
         }
-        
-        if(!this.renderable.isCurrentAnimation("walk")){
-            this.renderable.setCurrentAnimation("walk");
+        if (this.body.vel.x !== 0) {
+            if (!this.renderable.isCurrentAnimation("walk")) {
+                this.renderable.setCurrentAnimation("walk");
+            }
+
+        } else {
+            this.renderable.setCurrentAnimation("idle");
         }
-        
+
         this.body.update(delta);
-        
-        this._super(me.Entity, "update", [delta]); 
+
+        this._super(me.Entity, "update", [delta]);
         return true;
     }
 
 });
+
+game.PlayerBaseEntity = me.Entity.extend({
+    init: function(x, y, settings) {
+        this._super(me.Entity, 'init', [x, y, {
+                image: "tower",
+                width: 100,
+                height: 100,
+                spritewidth: "100",
+                spriteheight: "100",
+                getShape: function() {
+                    return(new me.Rect(0, 0, 100, 100)).toPolygon;
+                }
+            }]);
+        this.broken = false;
+    },
+    update: function() {
+
+    }
+
+})
